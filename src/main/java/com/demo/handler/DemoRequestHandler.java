2f6d46e6b57fef0db4bd39ebb7a9a637636ca510
yuyenews <631941527@qq.com>
2021-04-10 17:50:03 +0800

    首次提交

--- src/main/java/com/demo/handler/DemoRequestHandler.java
+++ src/main/java/com/demo/handler/DemoRequestHandler.java
@@ -3,21 +3,34 @@ package com.demo.handler;
 
 import com.mars.server.tcp.http.constant.MartianServerConstant;
 import com.mars.server.tcp.http.handler.ext.HttpRequestHandler;
+import com.mars.server.tcp.http.model.MarsFileUpLoad;
 import com.mars.server.tcp.http.request.MartianHttpRequest;
+
+import java.util.List;
+import java.util.Map;
 
 public class DemoRequestHandler implements HttpRequestHandler {
 
     @Override
     public void request(MartianHttpRequest martianHttpRequest) {
         // 如果是json格式提交的，就用这个方法取出参数字符串
-        System.out.println(martianHttpRequest.getJsonParam());
-        // 如果是其他方式提交的，就用这个方法去除参数
-        System.out.println(martianHttpRequest.getMarsParams());
-        // 如果是文件上传就用这个方法取出文件们
-        System.out.println(martianHttpRequest.getFiles());
+        String jsonStr = martianHttpRequest.getJsonParam();
+
+        /* *********如果是其他方式提交的，就用这个方法去除参数********* */
+        Map<String, List<String>> paramMap = martianHttpRequest.getMarsParams();
+        // 可以这样获取参数， 为什么类型是List? 因为http请求的参数name是可以重复的，一个Name可能获取到多个数据
+        List<String> list = paramMap.get("参数的name");
+
+        /* *********如果是文件上传就用这个方法取出文件们********* */
+        Map<String, MarsFileUpLoad> fileUpLoadMap = martianHttpRequest.getFiles();
+        // 可以这样获取到文件
+        MarsFileUpLoad marsFileUpLoad = fileUpLoadMap.get("参数的name");
+        marsFileUpLoad.getFileName();// 文件名
+        marsFileUpLoad.getInputStream(); // 文件流
+        marsFileUpLoad.getName();// 参数的name
 
         // 设置响应头
-        martianHttpRequest.getMartianHttpExchange().setResponseHeader(MartianServerConstant.CONTENT_TYPE, MartianServerConstant.JSON_CONTENT_TYPE);
+        martianHttpRequest.setResponseHeader(MartianServerConstant.CONTENT_TYPE, MartianServerConstant.JSON_CONTENT_TYPE);
         // 设置状态码和响应内容
         martianHttpRequest.getMartianHttpExchange().sendText(200, "ok");
     }
--- src/main/java/com/demo/server/DemoServer.java
+++ src/main/java/com/demo/server/DemoServer.java
@@ -10,12 +10,15 @@ public class DemoServer {
         try {
 
             MartianServer.builder()
-                    .bind(8080, 100)
-                    .fileSizeMax(100*1024*1024)
-                    .sizeMax(100*1024*1024)
-                    .threadPool(DemoThreadPool.getThreadPoolExecutor())
-                    .httpHandler("/",new DemoRequestHandler())
-                    .webSocketHandler("/websocket", new DemoSocketHandler())
+                    .bind(8080, 100)// 设置端口号和最大连接数
+                    .fileSizeMax(100*1024*1024)// 设置单个文件上传大小限制
+                    .sizeMax(100*1024*1024)// 设置上传文件总大小限制
+                    .readSize(1024) // 设置读取数据的缓冲区大小
+                    .readTimeout(10000)// 设置读取数据的超时时间
+                    .writeTimeout(1000)// 设置写入数据的超时时间
+                    .threadPool(DemoThreadPool.getThreadPoolExecutor())// 设置一个线程池
+                    .httpHandler("/",new DemoRequestHandler()) // 设置http的handler
+                    .webSocketHandler("/websocket", new DemoSocketHandler()) // 设置webSocket的handler
                     .start();
 
         } catch (Exception e){
