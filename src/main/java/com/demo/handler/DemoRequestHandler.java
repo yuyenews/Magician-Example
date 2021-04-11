ba2fcdda1a708382bc75f496868f69ed4c7e8ef5
yuyenews <631941527@qq.com>
2021-04-11 22:02:23 +0800

    首次提交

--- src/main/java/com/demo/handler/DemoRequestHandler.java
+++ src/main/java/com/demo/handler/DemoRequestHandler.java
@@ -10,21 +10,26 @@ public class DemoRequestHandler implements MagicianHandler {
 
     @Override
     public void request(MagicianRequest magicianRequest) {
+
+        for(String key : magicianRequest.getRequestHeaders().keySet()){
+            System.out.println(key+":"+magicianRequest.getRequestHeaders().get(key));
+        }
+
         // 如果是json格式提交的，就用这个方法获取参数字符串
         String jsonStr = magicianRequest.getJsonParam();
-
+        System.out.println(jsonStr);
         /* *********如果是其他方式提交的，就用这个方法获取参数********* */
         String list = magicianRequest.getParam("参数的name");
 
         /* *********如果是文件上传就用这个方法获取文件们********* */
         Map<String, MagicianFileUpLoad> fileUpLoadMap = magicianRequest.getFiles();
         // 可以这样获取到文件
-        MagicianFileUpLoad magicianFileUpLoad = fileUpLoadMap.get("参数的name");
+        MagicianFileUpLoad magicianFileUpLoad = fileUpLoadMap.get("file2");
         magicianFileUpLoad.getFileName();// 文件名
         magicianFileUpLoad.getInputStream(); // 文件流
         magicianFileUpLoad.getName();// 参数的name
 
-        // 设置响应头
+        // 设置响应数据
         magicianRequest.getResponse()
                 .setResponseHeader("content-type", "application/json;charset=UTF-8")
                 .sendText(200, "ok");
